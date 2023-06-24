056592e5cadd8fe4b39a7cbb12104a5aca8bff92
abrarabedin001 <abrarabedin001@gmail.com>
2023-06-24 14:26:12 +0600

    done with user create form

--- src/pages/loginHome/index.tsx
+++ src/pages/loginHome/index.tsx
@@ -1,20 +1,7 @@
 import * as React from 'react';
-import {
-  styled,
-  useTheme,
-  type Theme,
-  type CSSObject,
-} from '@mui/material/styles';
+
 import Box from '@mui/material/Box';
-import MuiDrawer from '@mui/material/Drawer';
-import MuiAppBar, {
-  type AppBarProps as MuiAppBarProps,
-} from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import CssBaseline from '@mui/material/CssBaseline';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
+
 import Courses from '~/components/Courses';
 import HeaderSidebar from '~/components/HeaderSidebar';
 
--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,78 +1,119 @@
-import { type NextPage } from 'next';
-import Head from 'next/head';
-import Link from 'next/link';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-nocheck
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import { signIn, signOut, useSession } from 'next-auth/react';
-import { api } from '~/utils/api';
-import { useEffect, useRef, useState } from 'react';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { type RouterOutputs } from '~/utils/api';
-import { TextField } from '@mui/material';
-import '@fontsource/roboto/700.css';
-// import { trpc } from "../utils/trpc";
+import TextField from '@mui/material/TextField';
+import Container from '@mui/material/Container';
+import Select from '@mui/material/Select';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Autocomplete from '@mui/material/Autocomplete';
+import axios from 'axios';
 
-type Todo = RouterOutputs['task']['getAll'][0];
+const validationSchema = yup.object({
+  name: yup
+    .string('Enter your name')
+    .min(3, 'Password should be of minimum 3 characters length')
+    .required('Name is required'),
+  email: yup
+    .string('Enter your email')
+    .email('Enter a valid email')
+    .required('Email is required'),
+  password: yup
+    .string('Enter your password')
+    .min(8, 'Password should be of minimum 8 characters length')
+    .required('Password is required'),
+  type: yup.string('Enter you type').required('Type is required'),
+});
 
-const Todo = () => {
-  const utils = api.useContext();
-
-  const ref = useRef<HTMLInputElement>(null);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [state, setState] = useState(true);
-
-  // const queryClient = useQueryClient();
-
-  const { mutateAsync, isLoading } = api.auth.insert.useMutation();
+const SignUp = () => {
+  const type = [
+    { label: 'TEACHER', year: 1994 },
+    { label: 'STUDENT', year: 1972 },
+  ];
+  const formik = useFormik({
+    initialValues: {
+      name: 'name',
+      email: 'foobar@example.com',
+      password: 'foobar',
+      type: '',
+    },
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    validationSchema: validationSchema,
+    onSubmit: async (values) => {
+      try {
+        console.log(values);
+        const user = await axios.post(
+          'http://localhost:5000/users/signup',
+          values,
+        );
+      } catch (err) {
+        console.log(err.message);
+      }
+    },
+  });
 
   return (
-    <Box
-      sx={{ minWidth: 275 }}
-      className=" flex flex-col items-center justify-center p-5 sm:w-full"
-    >
-      <Card
-        variant="outlined"
-        className=" flex flex-col items-center justify-center pt-5"
-      >
-        <Typography className="pt-3" variant="h4" component="h4">
-          {' '}
-          Sign Up{' '}
-        </Typography>
-        <CardContent className=" m-3 mt-5 flex flex-col items-center justify-center sm:w-full">
-          <TextField
-            id="outlined-basic"
-            label="Email"
-            variant="outlined"
-            margin="normal"
-          />
+    <Container>
+      <form onSubmit={formik.handleSubmit} className="mt-7 space-y-3">
+        <TextField
+          fullWidth
+          id="name"
+          name="name"
+          label="Name"
+          value={formik.values.name}
+          onChange={formik.handleChange}
+          error={formik.touched.name && Boolean(formik.errors.name)}
+          helperText={formik.touched.name && formik.errors.name}
+        />
+        <TextField
+          fullWidth
+          id="email"
+          name="email"
+          label="Email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          error={formik.touched.email && Boolean(formik.errors.email)}
+          helperText={formik.touched.email && formik.errors.email}
+        />
 
-          <TextField
-            id="outlined-basic"
-            label="Password"
-            variant="outlined"
-            margin="normal"
-          />
-
-          <TextField
-            id="outlined-basic"
-            label="Repeat Password"
-            variant="outlined"
-            margin="normal"
-          />
-          <CardActions>
-            <Button size="small" variant="contained">
-              <p className="font-bold">Sign Up</p>
-            </Button>
-          </CardActions>
-        </CardContent>
-      </Card>
-    </Box>
+        <InputLabel id="demo-simple-select-label">
+          Are you a teacher or student?
+        </InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="type"
+          name="type"
+          label="type"
+          value={formik.values.type}
+          onChange={formik.handleChange}
+          error={formik.touched.type && Boolean(formik.errors.type)}
+          helperText={formik.touched.type && formik.errors.type}
+        >
+          <MenuItem value={'TEACHER'}>TEACHER</MenuItem>
+          <MenuItem value={'STUDENT'}>STUDENT</MenuItem>
+        </Select>
+        <TextField
+          fullWidth
+          id="password"
+          name="password"
+          label="Password"
+          type="password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          error={formik.touched.password && Boolean(formik.errors.password)}
+          helperText={formik.touched.password && formik.errors.password}
+        />
+        <Button color="primary" variant="contained" type="submit" fullWidth>
+          Submit
+        </Button>
+      </form>
+    </Container>
   );
 };
 
-export default Todo;
+export default SignUp;
