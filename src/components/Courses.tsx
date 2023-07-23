99dcc99224188347b391a6242070e3ef09729827
abrarabedin001 <abrarabedin001@gmail.com>
2023-07-23 17:05:37 +0600

    fixed the single card issue

--- src/components/Courses.tsx
+++ src/components/Courses.tsx
@@ -1,15 +1,21 @@
-import { Grid } from '@mui/material';
+import { Grid, Box } from '@mui/material';
 import CourseCard from './CourseCard';
+import { Container } from 'postcss';
 
 const Courses = ({ courses }: { courses?: any[] }) => {
+  // If courses is undefined, set it to an empty array
+  console.log('courses', typeof courses);
+  const coursesArray: typeof courses = courses;
   return (
-    <Grid container spacing={2}>
-      {courses.map((course) => (
-        <Grid item xs={4} key={course.id}>
-          <CourseCard course={course} />
-        </Grid>
-      ))}
-    </Grid>
+    <Box className="m-3 p-3">
+      <Grid container spacing={2}>
+        {coursesArray!.map((course) => (
+          <Grid item key={course.id}>
+            <CourseCard course={course} />
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
   );
 };
 
