ce2c8525ae9c5438893f6c1c6fc80113a4efd76d
abrarabedin001 <abrarabedin001@gmail.com>
2023-08-02 04:55:16 +0600

    .

--- src/pages/tutors/[slug]/index.tsx
+++ src/pages/tutors/[slug]/index.tsx
@@ -1,132 +1,90 @@
-// import * as React from 'react';
+import * as React from 'react';
 
-// import Box from '@mui/material/Box';
+import Box from '@mui/material/Box';
 
-// import Courses from '~/components/Courses';
-// import HeaderSidebar from '~/components/HeaderSidebar';
-// import { CookiesProvider, useCookies } from 'react-cookie';
-// import axios from 'axios';
-// import Menu from '~/components/Menu';
-// import Container from '@mui/material/Container';
-// import Footer from '~/components/Footer';
-// import TextField from '@mui/material/TextField';
-// import { Button } from '@mui/material';
+import Courses from '~/components/Courses';
+import HeaderSidebar from '~/components/HeaderSidebar';
+import { CookiesProvider, useCookies } from 'react-cookie';
+import axios from 'axios';
+import Menu from '~/components/Menu';
+import Container from '@mui/material/Container';
+import Footer from '~/components/Footer';
+import TextField from '@mui/material/TextField';
+import { Button } from '@mui/material';
+import { ro } from 'date-fns/locale';
+import { useRouter } from 'next/router';
 
-// export default function LoginHome() {
-//   const [cookies, setCookie] = useCookies(['data']);
-//   const [courses, setCourses] = React.useState([]);
-//   const [searchedCourses, setSearchedCourses] = React.useState([]);
-//   const [query, setQuery] = React.useState('');
-//   const search = React.useRef('');
-//   React.useEffect(() => {
-//     console.log('cookies', cookies?.data?.user?.type);
-//     if (!cookies?.data?.user) {
-//       window.location.href = '/';
-//     }
+export default function LoginHome() {
+  const [cookies, setCookie] = useCookies(['data']);
+  const [courses, setCourses] = React.useState([]);
+  const [searchedCourses, setSearchedCourses] = React.useState([]);
+  const [query, setQuery] = React.useState('');
+  const search = React.useRef('');
+  const router = useRouter();
 
-//     const fetchCourses = async () => {
-//       try {
-//         const link = 'http://localhost:5000/course/showcourse';
-//         const response = await axios.get(link, {
-//           headers: {
-//             'content-type': 'application/json',
-//             Authorization: `token ${cookies?.data?.token}`,
-//           },
-//         });
-//         console.log('kaj kore ki?', response.data.courseshow);
-//         setCourses(response.data.courseshow);
-//       } catch (error) {
-//         console.error('Error fetching courses:', error);
-//       }
-//     };
+  React.useEffect(() => {
+    const fetchCourses = async () => {
+      try {
+        if (!router.isReady) return;
+        console.log('router', router.query.slug);
+        const link =
+          'http://localhost:5000/tutor/searchtutor/' + router.query.slug;
+        console.log('link', link);
+        const response = await axios.get(link, {
+          headers: {
+            'content-type': 'application/json',
+            Authorization: `token ${cookies?.data?.token}`,
+          },
+        });
+        console.log('Teachers course?', response.data.data);
+        setCourses(response.data.data);
+      } catch (error) {
+        console.error('Error fetching courses:', error);
+      }
+    };
 
-//     fetchCourses();
-//   }, [cookies]);
+    fetchCourses();
+  }, [router.isReady]);
 
-//   const searchCourse = async () => {
-//     try {
-//       const link =
-//         'http://localhost:5000/course/SearchTutor/' +
-//         router.asPath.split('course/SearchTutor/')[1];
-//       console.log(search.current.value);
-//       const response = await axios.get(
-//         link,
+  const searchCourse = async () => {
+    try {
+      const link =
+        'http://localhost:5000/course/categories/' + search.current.value;
+      console.log(search.current.value);
+      const response = await axios.get(
+        link,
 
-//         {
-//           headers: {
-//             'content-type': 'application/json',
-//             Authorization: `token ${cookies?.data?.token}`,
-//           },
-//         },
-//       );
-//       console.log('kaj kore ki?', response.data.coursedetails);
+        {
+          headers: {
+            'content-type': 'application/json',
+            Authorization: `token ${cookies?.data?.token}`,
+          },
+        },
+      );
+      console.log('kaj kore ki?', response.data.coursedetails);
 
-//       setSearchedCourses(response.data.coursedetails);
-//     } catch (error) {
-//       console.error('Error fetching courses:', error);
-//     }
-//   };
+      setSearchedCourses(response.data.coursedetails);
+    } catch (error) {
+      console.error('Error fetching courses:', error);
+    }
+  };
 
-//   console.log('where are my courses', courses);
-//   return (
-//     <CookiesProvider>
-//       <Menu />
-//       <Container>
-//         <Box className="flex  w-full justify-center space-x-5 p-4">
-//           <TextField
-//             id="outlined-basic"
-//             inputRef={search}
-//             variant="filled"
-//             size="small"
-//             onChange={(e) => {
-//               setQuery(e.target.value);
-//             }}
-//           />
-//           <button
-//             className="rounded bg-blue-800 p-2 text-white"
-//             onClick={() => {
-//               searchCourse();
-//             }}
-//           >
-//             Searched courses
-//           </button>
-//           {/* <button
-//             className="rounded bg-blue-800 p-3 text-white"
-//             onClick={() => {
-//               console.log(searchedCourses);
-//             }}
-//           >
-//             Search
-//           </button> */}
-//         </Box>
-//         {query == '' ? (
-//           <Box
-//             sx={{
-//               display: 'flex',
-//               flexDirection: 'column',
-//               minHeight: '1000px',
-//             }}
-//           >
-//             <Courses courses={courses}></Courses>
-//           </Box>
-//         ) : (
-//           ' '
-//         )}
-//         {/* {query != '' ? (
-//           <Box
-//             sx={{
-//               display: 'flex',
-//               flexDirection: 'column',
-//               minHeight: '1000px',
-//             }}
-//           >
-//             <Courses courses={searchedCourses}></Courses>
-//           </Box>
-//         ) : (
-//           ''
-//         )} */}
-//       </Container>
-//       <Footer />
-//     </CookiesProvider>
-//   );
-// }
+  console.log('where are my courses', courses);
+  return (
+    <CookiesProvider>
+      <Menu />
+      <Container>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            minHeight: '1000px',
+          }}
+        >
+          <Courses courses={courses}></Courses>
+        </Box>
+      </Container>
+      <Footer />
+    </CookiesProvider>
+  );
+}
