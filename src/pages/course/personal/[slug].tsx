afe5c9f5e4fa0f25709a5838624528f8100f1d50
abrarabedin001 <abrarabedin001@gmail.com>
2023-08-02 16:44:23 +0600

    .

--- src/pages/course/personal/[slug].tsx
+++ src/pages/course/personal/[slug].tsx
@@ -16,6 +16,7 @@ import Menu from '~/components/Menu';
 import CommentForm from '~/components/CommentForm';
 import ClassList from '~/components/ClassList';
 import Footer from '~/components/Footer';
+import { is } from 'date-fns/locale';
 
 const EditCourse = () => {
   const [cookies, setCookie] = useCookies(['user']);
@@ -51,6 +52,22 @@ const EditCourse = () => {
   }, [change, router.isReady]);
   console.log('data1', data1);
 
+  React.useEffect(() => {
+    if (data1) {
+      console.log('course enroll', data1?.course?.CourseEnroll);
+      const student = data1?.course?.CourseEnroll.filter(
+        (el) => el.StudentProfile.user.id === cookies?.data?.user?.id,
+      );
+      if (student?.length == 1) {
+        setIsStudent(true);
+      }
+
+      data1?.course?.TeacherProfile.user.id === cookies?.data?.user?.id
+        ? setIsTeacher(true)
+        : setIsTeacher(false);
+    }
+  }, [data1]);
+
   // console.log(
   //   'data1',
   //   data1.course.CourseEnroll.filter(
@@ -78,44 +95,52 @@ const EditCourse = () => {
               TeacherProfile={data1?.course?.TeacherProfile}
               classes={'w-full flex-col justify-center space-y-2 '}
             ></CourseMainBody>
-            <Box className="mb-5 mt-5 flex w-full justify-center space-x-3 bg-blue-400 p-5 shadow-md">
-              <button
-                className="rounded-md bg-white p-2 hover:scale-105"
-                onClick={() => {
-                  setOpen('comment');
-                }}
-              >
-                Comments
-              </button>
-              <button
-                className="rounded-md bg-white p-2 hover:scale-105"
-                onClick={() => {
-                  console.log('class list');
-                  setOpen('class-list');
-                }}
-              >
-                Class List
-              </button>
-              <button
-                className="rounded-md bg-white p-2 hover:scale-105"
-                onClick={() => {
-                  setOpen('chat');
-                }}
-              >
-                Chats
-              </button>
-            </Box>
+            {isTeacher || isStudent ? (
+              <Box className="mb-5 mt-5 flex w-full justify-center space-x-3 bg-blue-400 p-5 shadow-md">
+                <button
+                  className="rounded-md bg-white p-2 hover:scale-105"
+                  onClick={() => {
+                    setOpen('comment');
+                  }}
+                >
+                  Comments
+                </button>
+                <button
+                  className="rounded-md bg-white p-2 hover:scale-105"
+                  onClick={() => {
+                    console.log('class list');
+                    setOpen('class-list');
+                  }}
+                >
+                  Class List
+                </button>
+                <button
+                  className="rounded-md bg-white p-2 hover:scale-105"
+                  onClick={() => {
+                    setOpen('chat');
+                  }}
+                >
+                  Chats
+                </button>
+              </Box>
+            ) : (
+              ''
+            )}
+
             <Box className="flex w-full ">
               {open === 'comment'
                 ? data1?.course?.id && <CommentForm id={data1?.course?.id} />
                 : ''}
 
-              {open === 'class-list'
-                ? data1?.course?.id && <ClassList id={data1?.course?.id} />
+              {isTeacher || isStudent
+                ? open === 'class-list'
+                  ? data1?.course?.id && <ClassList id={data1?.course?.id} />
+                  : ''
                 : ''}
-
-              {open === 'chat'
-                ? data1?.course?.id && <Chat id={data1?.course?.id} />
+              {isTeacher || isStudent
+                ? open === 'chat'
+                  ? data1?.course?.id && <Chat id={data1?.course?.id} />
+                  : ''
                 : ''}
             </Box>
           </Box>
